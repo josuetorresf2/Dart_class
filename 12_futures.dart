eb10985f55461f951429cc8ae4e10d995eec6350
Joshue Torres <josuetorresf2@gmail.com>
2023-06-12 12:39:49 -0500

    futures

---
+++ 12_futures.dart
@@ -0,0 +1,28 @@
+/*
+Un Future representa principalmente el resultado de
+una operación asíncrona. Es una promesa de que pronto
+tendrás un valor. La promesa puede fallar y hay que manejar 
+la excepción. Los futures son un acuerdo de que en el futuro 
+tendrás un valor para ser usado.
+ */
+
+void main() {
+  print('Inicio del programa');
+
+  httpGet('http://fernando-herrera.com/cursos')
+      .then((value) => print(value))
+      .catchError((err) {
+    print('Error: $err');
+  });
+
+  print('Fin del programa');
+}
+
+Future<String> httpGet(String url) {
+  return Future.delayed(const Duration(seconds: 1), () {
+    throw 'Error en la peticion http';
+
+    //return 'Repuesta de la peticion http'; //linea comentada
+  });
+  //() estop se agrega por que pasamos una funcion
+}
